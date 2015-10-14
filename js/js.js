46c2600e442fef2ba64a40776ca2a0e8a219ed9e
tyler <tylerhines1992>
2015-10-14 15:29:05 -0400

    stopped modal from closing on error

--- js/js.js
+++ js/js.js
@@ -14,9 +14,24 @@ Contact.prototype.greet = function() {
 //var tyler = new Contact("Tyler", "555-555-5555");
 //var mike = new Contact("mike", "555-555-5555");
 
-$("#clickToAdd").click(function() {
+$("#clickToAdd").click(function(e) {
+    validName();
     var newName = document.getElementById("addName").value;
     var newNumber = document.getElementById("addNumber").value;
     newName = new Contact(newName, newNumber);
     $("#output").append("<p>" + "Name: " + newName.name + "</p>" + "<p>" + "Number: " + newName.number + "</p>");
 });
+
+function validName(e) {
+    var nameRegEx = /[^a-z]/ig;
+    var name = document.getElementById("addName").value;
+
+    if (name == null || name == "") {
+        alert("Enter a real name");
+    } else {
+        $("#myModal").modal("hide");
+        $("#myModal").on("hidden.bs.modal", function() {
+            $("#addName").val("");
+        })
+    }
+};
