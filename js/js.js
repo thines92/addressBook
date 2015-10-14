ae184a841614672b3b6389fbc5a05d1ea3e105dd
tyler <tylerhines1992>
2015-10-14 13:00:36 -0400

    added newName and newNumber append on output. still no enter functionality

--- js/js.js
+++ js/js.js
@@ -6,29 +6,18 @@ var Contact = function(name, number) {
     this.name = name;
     this.number = number;
 }
+
 Contact.prototype.greet = function() {
     document.getElementById("output").innerHTML = this.name + " " + this.number;
-}
+};
 
-var tyler = new Contact("Tyler", "555-555-5555");
+//var tyler = new Contact("Tyler", "555-555-5555");
+//var mike = new Contact("mike", "555-555-5555");
 
-function createContact(e) {
-    var target = e.target;
-    var contactName = target.value;
-
-    new Contact(contactName, "unknown");
-}
 
 $("#addButton").click(function() {
-    mike.greet();
-});
-
-$("#addTextBox").bind("enterKey", function(e){
-    createContact();
-})
-
-$("#addTextBox").keyup(function(e){
-    if(e.keyCode == 13) {
-        $(this).trigger("enterKey");
-    }
+    var newName = document.getElementById("addName").value;
+    var newNumber = document.getElementById("addNumber").value;
+    newName = new Contact(newName, newNumber);
+    $("#output").append("<p>" + "Name: " + newName.name + "</p>" + "<p>" + "Number: " + newName.number + "</p>");
 });
